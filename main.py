75437edabd9ce8bd2e4d9e92c9d9bd27dccf3524
Deborah9494 <deborah94.iori@gmail.com>
2025-11-23 21:02:25 +0100

    Add bandit

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 
+password = "123" # Hardcoded password
+
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"mensaje": "Hola desde Koyeb"}
+
+@app.get("/health")
+def health():
+    return{"status":"ok"}
